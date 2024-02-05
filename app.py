51592dde7c9ec9d8e03efa71dea3d694c2b97981
VanditGupta <vanditgupta22@gmail.com>
2024-02-05 13:42:10 -0500

    Added utility functions for YAML

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 # To run this file, use the following command:
 # python3.11 app.py
 
+import sys
 from cellSegmentation.logger import logging
+from cellSegmentation.exception import AppException
 
 logging.info(" Custom logging message")
+
+# try:
+#     a = 4 / "6"
+
+# except Exception as e:
+#     raise AppException(e, sys)
